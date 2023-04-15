d8e7c791b0a66e4a2a7b2732d35a02038a07c305
Louis <louisrca@MacBook-Pro-de-Louis.local>
2023-04-16 00:21:40 +0200

    integration url google map

--- front-end/src/Views/city/City.js
+++ front-end/src/Views/city/City.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Iframe from "react-iframe";
 import style from "./City.module.css";
+
 import CityDescription from "../../Composants/CityDescription/CityDescription.tsx";
 
 export default function City() {
@@ -10,6 +12,20 @@ export default function City() {
     <div className={style.cityPage}>
       <div className={style.headCityDescription}>
         <CityDescription city={cityParams} country={countryParams} />
+      </div>
+      <div className={style.googleMapStatic}>
+        <Iframe
+          width="650"
+          height="350"
+          frameBorder="0"
+          style="border:0"
+          referrerPolicy="no-referrer-when-downgrade"
+          src={
+            "https://www.google.com/maps/embed/v1/place?key=AIzaSyA2PyvD7spTT0DJhYD6g7EbfLgzBrHBSGQ&q=" +
+            countryParams
+          }
+          allowFullScreen
+        ></Iframe>
       </div>
     </div>
   );
